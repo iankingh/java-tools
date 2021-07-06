a3fd38d1f92f99b7304a186fd4545279e789f76f
ianhuang <ianaa10716@gmail.com>
2021-07-06 23:29:12 +0800

    修改Test

--- src/main/java/com/ian/tools/ToolsApplication.java
+++ src/main/java/com/ian/tools/ToolsApplication.java
@@ -3,6 +3,20 @@ package com.ian.tools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 
+ * 
+ * 本類別詳細說明 ToolsApplication
+ * 
+ * <p/>
+ * 
+ * @Description: ToolsApplication
+ * @author Ian
+ * @version 1.0, 2021年06月02日
+ * @see 
+ * 
+ */
 @SpringBootApplication
 public class ToolsApplication {
 
